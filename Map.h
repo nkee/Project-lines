35f015988c05002072f6ed31d115f7c096fc7cd1
nkee <nkee-i@mail.ru>
2014-03-27 13:53:36 +0200

    Create Map.h

---
+++ Map.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Obyect.h"
+#include "Cell.h"
+#include <irrlicht.h>
+
+using namespace irr;
+
+using namespace core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
+
+class Map
+{
+public:
+	Cell mas[5][5];
+	Obyect lin[5][5];
+public:
+	Map(void);
+	~Map(void);
+	void line();
+	bool random();
+	void Move(float x, float y, float z, Cell current, Cell future);
+};
+
